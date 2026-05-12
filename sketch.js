d5069f56457ca0bf740460a5dbe31ccc0457d32d
mangocheesecat <jic7827@my.sduhsd.net>
2026-05-12 22:16:07 +0000

    papa's smoothie-land

--- sketch.js
+++ sketch.js
@@ -1,27 +1,37 @@
 await Canvas();
-world.gravity.y = 10;
+var screen = "start"
+if (screen === "start") {
+	abc();
+}
+function abc() {
+	textSize(32);
+	textAlign(CENTER, CENTER);
+	background("white")
+	fill("pink");
+	text("Papa's Smoothie-Land", 0, 0);
+}
 
-let ball = new Sprite();
-ball.diameter = 50;
-ball.img = '🤪';
+// world.gravity.y = 10;
 
-let groundA = new Sprite();
-groundA.x = -120;
-groundA.width = 220;
-groundA.rotation = 30;
-groundA.physics = STATIC;
+// let ball = new Sprite();
+// ball.diameter = 50;
+// ball.img = '😉';
 
-let groundB = new Sprite();
-groundB.x = 120;
-groundB.width = 220;
-groundB.rotation = -30;
-groundB.physics = STATIC;
+// let groundA = new Sprite();
+// groundA.x = -120;
+// groundA.width = 220;
+// groundA.rotation = 30;
+// groundA.physics = STATIC;
 
-q5.update = function () {
-	background('skyblue');
-	text('click to jump!', 0, -50);
+// let groundB = new Sprite();
+// groundB.x = 120;
+// groundB.width = 220;
+// groundB.rotation = -30;
+// groundB.physics = STATIC;
 
-	if (mouse.presses()) ball.vel.y = -5;
-};
+// q5.update = function () {
+// 	background('blue');
+// 	text('click to jump!', 0, -50);
 
-MEOW
+// 	if (mouse.presses()) ball.vel.y = -5;
+// };
