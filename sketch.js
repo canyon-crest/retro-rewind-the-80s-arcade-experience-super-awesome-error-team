18cb8c0d3660fc5771359cc0836f0061b01ef6d8
mangocheesecat <jic7827@my.sduhsd.net>
2026-05-13 21:47:37 +0000

    meow

--- sketch.js
+++ sketch.js
@@ -8,38 +8,44 @@ function abc() {
 	textAlign(CENTER, CENTER);
 	background("white")
 	fill("pink");
-	text("Papa's Smoothie-Land", 0, 0);
-}
+	text("Papa's Smoothie-Land", 0, -25);
+	text("Click anywhere to begin", 0, 25);
+	if (mouseIsPressed) {
+		playScreen();
+	};
+};
 
-function endScren() {
+function playScreen() {
+world.gravity.y = 10;
+
+let ball = new Sprite();
+ball.diameter = 50;
+ball.img = '🤯';
+
+let groundA = new Sprite();
+groundA.x = -120;
+groundA.width = 220;
+groundA.rotation = 30;
+groundA.physics = STATIC;
+
+let groundB = new Sprite();
+groundB.x = 120;
+groundB.width = 220;
+groundB.rotation = -30;
+groundB.physics = STATIC;
+
+q5.update = function () {
+	background('blue');
+	text('click to jump!', 0, -50);
+
+	if (mouse.presses()) ball.vel.y = -5;
+};
+};
+
+function endScreen() {
 	textSize(32);
 	textAlign(CENTER, CENTER);
 	background("white")
 	fill("pink");
 	text("Congratulations! This is the end :)", 0, 0);
-}
-
-// world.gravity.y = 10;
-
-// let ball = new Sprite();
-// ball.diameter = 50;
-// ball.img = '😉';
-
-// let groundA = new Sprite();
-// groundA.x = -120;
-// groundA.width = 220;
-// groundA.rotation = 30;
-// groundA.physics = STATIC;
-
-// let groundB = new Sprite();
-// groundB.x = 120;
-// groundB.width = 220;
-// groundB.rotation = -30;
-// groundB.physics = STATIC;
-
-// q5.update = function () {
-// 	background('blue');
-// 	text('click to jump!', 0, -50);
-
-// 	if (mouse.presses()) ball.vel.y = -5;
-// };
+};
